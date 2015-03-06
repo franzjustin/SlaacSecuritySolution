14d0d3b5abab2d31c36679dba07e6e1cf07996f9
hecchi777 <franz_buenaventura@hotmail.com>
2015-03-05 19:25:18 -0800

    added flask

--- ClassS3/Sniff.py
+++ ClassS3/Sniff.py
@@ -1,4 +1,7 @@
 import pcapy
+import time
+import threading
+from threading import Thread
 from pcapy import findalldevs
 from datetime import datetime
 from decimal import *
@@ -9,108 +12,126 @@ from ClassS3 import  SendPackets
 from ClassS3 import  SLAAC_Message
 
 
-def getInterface():
-    ifs = findalldevs()
-    if 0 == len(ifs):
-        print "You don't have enough permissions to open any interface on this system."
-        sys.exit(1)
+class Forever_Loop(threading.Thread):
+    def __init__(self):
+        self.isRunning = True
+        Thread.__init__(self)
 
-    # Only one interface available, use it.
-    elif 1 == len(ifs):
-        print 'Only one interface present, defaulting to it.'
-        return ifs[0]
+    def start(self):
+        Thread.__init__(self)
+        Thread.start(self)
 
-    # Ask the user to choose an interface from the list.
-    count = 0
-    for iface in ifs:
-        print '%i - %s' % (count, iface)
-        count += 1
-    idx = int(raw_input('Please select an interface: '))
+    def stop(self):
+        self.isRunning = False
 
-    return ifs[idx]
+    def setExpression(self, expression):
+        self.expression = expression
+
+    def run(self):
+        Thread.__init__(self)
+        while self.isRunning == True:
+            def getInterface():
+                ifs = findalldevs()
+                if 0 == len(ifs):
+                    print "You don't have enough permissions to open any interface on this system."
+                    sys.exit(1)
+
+                # Only one interface available, use it.
+                elif 1 == len(ifs):
+                    print 'Only one interface present, defaulting to it.'
+                    return ifs[0]
+
+                # Ask the user to choose an interface from the list.
+                count = 0
+                for iface in ifs:
+                    print '%i - %s' % (count, iface)
+                    count += 1
+                idx = int(raw_input('Please select an interface: '))
+
+                return ifs[idx]
 
 
 
 
-    #list all the network devices
+                #list all the network devices
 
-pcapy.findalldevs()
-max_bytes = 1024
-promiscuous = False
-read_timeout = 100 # in milliseconds
-pc = pcapy.open_live(getInterface(), max_bytes, promiscuous, read_timeout)
-pc.setfilter('icmp6')
-mode = False
-parser = DataParse.Dataparse(mode)
-    # callback for received packets
+            pcapy.findalldevs()
+            max_bytes = 1024
+            promiscuous = False
+            read_timeout = 100 # in milliseconds
+            pc = pcapy.open_live(getInterface(), max_bytes, promiscuous, read_timeout)
+            pc.setfilter('icmp6')
+            mode = False
+            parser = DataParse.Dataparse(mode)
+                # callback for received packets
 
-def recv_pkts(hdr, data):
-    try:
-        mode = False
-        eth = EthDecoder().decode(data)
-        ethChild = eth.child()
-        ethChild2 = ethChild.child()
-        if  ethChild2.get_type() == 134:
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_sniff",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #-----------------------------------
-            parser.sniffSlaac(data)
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_detect",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #-----------------------------------
-            #print "Hello"
+            def recv_pkts(hdr, data):
+                try:
+                    mode = False
+                    eth = EthDecoder().decode(data)
+                    ethChild = eth.child()
+                    ethChild2 = ethChild.child()
+                    if  ethChild2.get_type() == 134:
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_sniff",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #-----------------------------------
+                        parser.sniffSlaac(data)
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_detect",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #-----------------------------------
+                        #print "Hello"
 
-        elif  ethChild2.get_type() == 135:
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_sniff",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #-----------------------------------
-            parser.sniffSlaac(data)
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_detect",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #-----------------------------------
-            #print "Hello"
-        elif  ethChild2.get_type() == 136:
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_sniff",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #----------0-------------------------
-            parser.sniffSlaac(data)
-            #------------Time Start------------
-            test_open = open("../TestFiles/realtime_test1_detect",'a')
-            test_start = datetime.now()
-            sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
-            test_open.write(str(sum))
-            test_open.write('\n')
-            test_open.close()
-            #-----------------------------------
-            #print "Hello"
+                    elif  ethChild2.get_type() == 135:
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_sniff",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #-----------------------------------
+                        parser.sniffSlaac(data)
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_detect",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #-----------------------------------
+                        #print "Hello"
+                    elif  ethChild2.get_type() == 136:
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_sniff",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #----------0-------------------------
+                        parser.sniffSlaac(data)
+                        #------------Time Start------------
+                        test_open = open("../TestFiles/realtime_test1_detect",'a')
+                        test_start = datetime.now()
+                        sum = Decimal(test_start.strftime(("%s"))) + Decimal(test_start.strftime(("%f")))/1000000
+                        test_open.write(str(sum))
+                        test_open.write('\n')
+                        test_open.close()
+                        #-----------------------------------
+                        #print "Hello"
 
-    except:
-        x = 1
-        print "error"
-packet_limit = -1 # infinite
-pc.loop(packet_limit, recv_pkts) # capture packets
+                except:
+                    x = 1
+                    print "error"
+            packet_limit = -1 # infinite
+            pc.loop(packet_limit, recv_pkts) # capture packets
--- ClassS3/demo.py
+++ ClassS3/demo.py
@@ -1,62 +1,137 @@
 import flask, flask.views
 import os
 import functools
+import threading
+import Sniff
+import time
 
+# from sniffer import StoppableThread
+
+#snifferFile = StoppableThread()
+
+#!/usr/bin/python
+
+
+import thread
+
+APP_ROOT = os.path.dirname(os.getcwd())
+APP_STATIC = os.path.join(APP_ROOT, 'Logs')
+l = Sniff.Forever_Loop()  #the python file class with the function of forever loop / to use as a thread
+global learning
+global running
+learning = False
+running = False
 app = flask.Flask(__name__)
+
 app.secret_key = "bacon"
+users = {'admin': 'admin'}
 
-users = {'matt':'bacon'}
 
-class Main(flask.views.MethodView):
-	def get(self):
-		#print 'hello world!'
-		return flask.render_template('index.html')
+class Main(flask.views.MethodView):  # the main page
+	def get(self):  # when open, this is the first page it gets
+		print str(APP_ROOT)
+		print str(running)
+		return flask.render_template('index.html', running=running)  #flask uses templates of html files for the interface // in this case, the index page
 
 	def post(self):
 		if 'logout' in flask.request.form:
 			flask.session.pop('username', None)
+			global running
+			running = False
+			l.stop()
 			return flask.redirect(flask.url_for('index'))
-		required =['username', 'passwd']
+		required = ['username', 'password']
 		for r in required:
 			if r not in flask.request.form:
-				flask.flash("Error: {0} is required.".format(r))
+				flask.flash("Error {0} is required.".format(r))
 				return flask.redirect(flask.url_for('index'))
-		username = flask.request.form['username']
-		passwd = flask.request.form['passwd']
-		if username in users and users[username] == passwd:
-			flask.session['username'] = username
-		else:
-			flask.flash("Username doesn't exist or incorrect password")
-		return flask.redirect(flask.url_for('index'))
+			username = flask.request.form['username']
+			password = flask.request.form['password']
+			if username in users and users[username] == password:
+				flask.session['username'] = username
+			else:
+				flask.flash("username doesn't exist or incorrect password")
+			return flask.redirect(flask.url_for('index'))
 
 def login_required(method):
 	@functools.wraps(method)
 	def wrapper(*args, **kwargs):
-		if 'username' in flask.session:		#checks to see if there is a username session
+		if 'username' in flask.session:
 			return method(*args, **kwargs)
 		else:
-			flask.flash("A login is required to see the page!")
+			flask.flash('A login is required to see the page')
 			return flask.redirect(flask.url_for('index'))
 	return wrapper
 
-class Remote(flask.views.MethodView):
+class Test(flask.views.MethodView):
+	def get(self):
+		return flask.render_template('test.html', running=running)
+
+
+class Stop(flask.views.MethodView):
 
 	@login_required
 	def get(self):
-		return flask.render_template('remote.html')
-	
+		global running
+		running = False
+		l.stop()
+		return flask.render_template('index.html', running=running)
+
+class Sniffer(flask.views.MethodView):
+	@login_required
+	def get(self):
+		print "went to sniffer, running is"
+		print str(running)
+		return flask.render_template('sniffer.html', running=running)
+
 	@login_required
 	def post(self):
-		result = eval(flask.request.form['expression'])
-		flask.flash(result)
-		return flask.redirect(flask.url_for('remote'))
+		global running
+		expression = str(flask.request.form['expression'])  # gets the input of the user
+		l.setExpression(expression)  # sets the input of the user to know where to sniff
+		try:
+			if l.isRunning == False: # if the thread has been shut down
+				l.isRunning = True  # change it to true, so it could loop again
+				running = True
+				l.start() # starts the forever loop / declared from the top to be a global variable
+				print str(running)
+			else:
+				running = True
+				print str(running)
+				l.start()
+		except Exception, e:
+			raise e
+		return flask.render_template('test.html', running=running) #goes to the test.html page
 
-app.add_url_rule('/', 
-	view_func=Main.as_view('index'), 
-	methods=['GET', 'POST'])
-app.add_url_rule('/remote/', 
-	view_func=Remote.as_view('remote'), 
-	methods=['GET', 'POST'])
+
+class Notif(flask.views.MethodView):
+	@login_required
+	def get(self):
+		logfile = "log_report-" + time.strftime('%Y%m%d') + ".s3"   # creates the string filename of the current log file used
+		#print logfile
+		global running
+		with open(os.path.join(APP_STATIC, logfile)) as f:
+			stat = str(f.read())
+			flask.flash(stat)
+			return flask.render_template('status.html', running = running)
+
+
+class Learn(flask.views.MethodView):
+	@login_required
+	def get(self):
+		return 0
+
+	@login_required
+	def post(self):
+		return 0
+
+
+app.add_url_rule('/', view_func=Main.as_view('index'), methods=['GET', 'POST'])
+app.add_url_rule('/sniffer', view_func=Sniffer.as_view('sniffer'), methods=['GET', 'POST'])
+app.add_url_rule('/test', view_func=Test.as_view('test'), methods=['GET'])
+app.add_url_rule('/stop', view_func=Stop.as_view('stop'), methods=['GET', 'POST'])
+app.add_url_rule('/notification', view_func=Notif.as_view('notif'), methods=['GET', 'POST'])
+app.add_url_rule('/learn', view_func=Learn.as_view('learn'), methods=['GET', 'POST'])
 
 app.debug = True
 app.run()
